57becc4ead7c417d6a92c0dd1b137db932fedbff
Palta2639 <134703201+Palta2639@users.noreply.github.com>
2023-06-30 11:37:49 -0400

    Add files via upload

---
+++ Padel.py
@@ -0,0 +1,24 @@
+opcion=[]
+NombreJugador=[]
+RutJugador=[]
+FechaNacimientoJugador=[]
+Categoria=[]
+Celular=[]
+IdentificadorDeParejas=[]
+print("Bienvenido!")
+print("~"*20)
+print("1-Grabar")
+print("2-Buscar")
+print("3-Imprimir parejas")
+print("4-Salir")
+int(input("Elija una opcion del 1 al 4: "))
+if opcion:=1:
+        input("Indique nombre del jugador:")
+        input("Indique rut del jugador (con guion): ")
+        input("Indique fecha de nacimiento Ej: 26 de abril de 1995: ")
+        input("Indique celular: ")
+        input("indique identificador de parejas: ")
+if opcion==2:
+        input("Rut del participante: ")
+if opcion==3:
+        input("Indique identificador de parejas:")
